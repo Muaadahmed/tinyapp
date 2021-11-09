9d3c39c700b171ac8deb08c54d3b5cba69778c33
Muaad Ahmed <muaad.abdi.ahmed@gmail.com>
2021-11-09 22:25:55 +0000

    Implemented a shortened route that redirects to the longhandURL based on the shorthandURL

--- express_server.js
+++ express_server.js
@@ -36,8 +36,15 @@ app.get('/urls/new', (req, res) => {
 });
 
 app.post('/urls', (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send('Ok');   // Respond with 'Ok' (we will replace this)    
+  let randomShortURL = generateRandomString();
+  urlDatabase[randomShortURL] = req.body.longURL;  // Log the POST request body to the console
+  res.redirect(`/urls/${randomShortURL}`);   // Respond with 'Ok' (we will replace this)    
+  console.log(urlDatabase)
+});
+
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
